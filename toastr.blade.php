505bbe37b6e6f06d6ad693a49e00b7a89d983e74
MOHD FAHAD AFTAB <144164866+Fahadaftab00@users.noreply.github.com>
2025-02-03 11:18:02 +0530

    Add files via upload

---
+++ toastr.blade.php
@@ -0,0 +1,31 @@
+
+{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
+ 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+
+<script>
+ 
+  @if(Session::has('success') || Session::has('error') || Session::has('fail') || Session::has('failed'))
+  {
+    @if(Session::has('success'))
+    toastr.success("{{ Session::get('success') }}");
+    @endif
+
+    @if(Session::has('error'))
+    toastr.error("{{ Session::get('error') }}");
+    @endif
+
+    @if(Session::has('fail'))
+    toastr.error("{{ Session::get('fail') }}");
+    @endif
+
+    @if(Session::has('failed'))
+    toastr.error("{{ Session::get('failed') }}");
+    @endif
+  }
+  @endif
+    
+  </script>
